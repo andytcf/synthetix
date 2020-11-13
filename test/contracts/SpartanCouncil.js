b3a5fcdc8c5c086af8b58baa15c8561b195b0339
AndyTCF <andy.c.f.98@gmail.com>
2020-11-13 16:44:01 +1100

    remove contracts

--- test/contracts/SpartanCouncil.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// const { contract, web3 } = require('@nomiclabs/buidler');
-
-// const { assert } = require('./common');
-
-// contract('SpartanCouncil @ovm-skip', async accounts => {});
-
